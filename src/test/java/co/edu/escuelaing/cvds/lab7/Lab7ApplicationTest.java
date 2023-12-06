5a5b24295324e15d66ecf8ff377665911f87b130
JuanalvarezECI <143049108+JuanalvarezECI@users.noreply.github.com>
2023-12-06 13:04:53 -0500

    Update Lab7ApplicationTest.java

--- src/test/java/co/edu/escuelaing/cvds/lab7/Lab7ApplicationTest.java
+++ src/test/java/co/edu/escuelaing/cvds/lab7/Lab7ApplicationTest.java
@@ -1,10 +1,4 @@
 package co.edu.escuelaing.cvds.lab7;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class Lab7ApplicationTest {
-  @Test
-	void contextLoads() {
-	}
 }
